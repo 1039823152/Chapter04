5fafc3a9ab902ff49e7fb7ac087bd47d4b7c64db
1039823152 <1039823152@qq.com>
2018-04-04 12:23:49 +0800

    444

--- src/main/java/com/bdqn/entity/test01.java
+++ src/main/java/com/bdqn/entity/test01.java
@@ -2,6 +2,6 @@ package com.bdqn.entity;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("fhdjfhdjfh");
+        System.out.println("修改");
     }
 }
